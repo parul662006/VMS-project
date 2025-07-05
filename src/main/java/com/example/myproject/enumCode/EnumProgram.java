095b58167b02608fa1872f0c928431001add5e51
parul662006 <parulkumawat666@gmail.com>
2025-07-05 22:19:16 +0530

    project deleted

--- src/main/java/com/example/myproject/enumCode/EnumProgram.java
+++
@@ -1,9 +0,0 @@
-package com.example.myproject.enumCode;
-
-public class EnumProgram {
-    public enum Role{
-        USER,
-        ADMIN
-    }
-}
-
--- src/main/java/com/example/myproject/globalException/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.myproject.globalException;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
